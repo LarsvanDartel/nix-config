655f433ad450e81465476e5397e8963e91f04b85
LarsvanDartel <larsvandartel73@gmail.com>
2025-03-17 12:48:50 +0100

    feat(nvim): removed unneccecary plugins

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -46,10 +46,8 @@ in {
           };
 
           visuals = {
-            nvim-scrollbar.enable = true;
             nvim-web-devicons.enable = true;
             nvim-cursorline.enable = true;
-            cinnamon-nvim.enable = true;
             fidget-nvim.enable = true;
             highlight-undo.enable = true;
             indent-blankline.enable = true;
