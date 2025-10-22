b5b4c4c0fb10b543fb886e3dea8679ab819b384b
LarsvanDartel <larsvandartel73@gmail.com>
2025-10-22 17:28:19 +0200

    feat: add caches

--- modules/home/desktops/hyprland/default.nix
+++ modules/home/desktops/hyprland/default.nix
@@ -24,6 +24,13 @@ in {
   };
 
   config = mkIf cfg.enable {
+    nix.settings = {
+      trusted-substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
+
     cli.programs = {
       clipse.enable = true;
     };
