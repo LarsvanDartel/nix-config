6d19195477135b8af2ed052d5bbf49f632b490da
LarsvanDartel <larsvandartel73@gmail.com>
2026-04-02 16:25:46 +0200

    feat(tmux): delete escape time

--- modules/home/cli/programs/tmux/default.nix
+++ modules/home/cli/programs/tmux/default.nix
@@ -16,6 +16,7 @@ in {
   config = mkIf cfg.enable {
     programs.tmux = {
       enable = true;
+      escapeTime = 0;
       clock24 = true;
       customPaneNavigationAndResize = true;
       disableConfirmationPrompt = true;
